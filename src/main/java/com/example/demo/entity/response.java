8f1de36f86969f879b0fc81225a880ae2d1bbc55
Sumi Sivakumar <sumisivakumar@Sumis-MacBook-Air.local>
2021-02-10 17:16:31 -0600

    First time Commit

---
+++ src/main/java/com/example/demo/entity/response.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+
+public class response implements Serializable{
+	
+	private String name;
+
+	public String getName() {
+	return name;
+	}
+
+	public void setName(String name) {
+	this.name = name;
+	}
+	
+	private int number;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+
+}
